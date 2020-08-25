7fbbd4bae29f89a9e78a75792d820d1a000d376c
Jeffesson Gomes <jeffesson01@gmail.com>
2020-08-25 03:49:26 -0300

    :art: Alterando estilo da pagina

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,36 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import { Title, Form, Repositories } from './styles';
+
+import logoImg from '../../assets/logo.svg';
 
 const Dashboard: React.FC = () => {
-  return <Title>Dashboard</Title>;
+  return (
+    <> 
+      <img src={logoImg} alt="Github Explorer"/>
+      <Title>Explore repositorios no Github</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositorio" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="">
+          <img src="https://avatars3.githubusercontent.com/u/17955358?s=460&u=ba042b3e183a3e36de57089bb11196ef3985de26&v=4" 
+          alt="Jeffesson Gomes"/>
+          <div>
+            <strong>Jeffesson/Rep</strong>
+            <p>Desc</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+
+    </>
+  );
 }
 
 export default Dashboard;
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,5 +1,7 @@
 import {createGlobalStyle} from 'styled-components';
 
+import githubBack from '../assets/github.svg';
+
 export default createGlobalStyle`
   * {
     margin: 0;
@@ -9,7 +11,7 @@ export default createGlobalStyle`
   }
 
   body{
-    background: #F0F0F5;
+    background: #F0F0F5 url(${githubBack}) no-repeat 70% top ;
     -webkit-font-smoothing: antialiased;
   }
 
